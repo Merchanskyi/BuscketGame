84c265d2990f2d04e54660d2ba562b4d31e9970b
Ruslan <rusya998@gmail.com>
2018-03-31 02:24:48 +0300

    Complete program

--- CABuscketGame/CABuscketGame/Program.cs
+++ CABuscketGame/CABuscketGame/Program.cs
@@ -25,8 +25,11 @@ namespace CABuscketGame
             int maxValue = 0;
             int quessNumber = 0;
 
-            Console.WriteLine("Игра BuscketGame");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("=====[Игра BuscketGame]=====");
+            Console.ResetColor();
 
+            #region SamCodeVvoda
             while (true)
             {
                 minValue = Tools.EnterNum("Введите Минимальное(minValue) число: ");
@@ -34,7 +37,9 @@ namespace CABuscketGame
                 if (minValue < 0)
                 {
                     Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
                     Console.WriteLine("Нельзя задать число меньше нуля для [Минимального(minValue)]. Попробуйте заново.");
+                    Console.ResetColor();
                     continue;
                 }
 
@@ -43,7 +48,9 @@ namespace CABuscketGame
                 if (maxValue <= minValue)
                 {
                     Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
                     Console.WriteLine("Нельзя задать число меньше минимального для [Максимального(maxValue)]. Попробуйте заново.");
+                    Console.ResetColor();
                     continue;
                 }
 
@@ -52,11 +59,16 @@ namespace CABuscketGame
                 if (quessNumber > maxValue || quessNumber < minValue)
                 {
                     Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
                     Console.WriteLine("Нельзя задать верное число вне диапазона для [Верного(maxValue)]. Попробуйте заново.");
+                    Console.ResetColor();
                     continue;
                 }
                 break;
             }
+            #endregion
+
+            #region AllPlayerMutyat
 
             //For full pass
             int pass = 0;
@@ -70,49 +82,63 @@ namespace CABuscketGame
             //For player 3
             int step3 = 0;
             int[] masForPlayer3 = new int[maxValue];
+            int temp3 = 0;
+
+            //For player 4
+            int step4 = 0;
+
+            //For player 5
+            int step5 = 0;
 
             while (true)
             {
-                //for (int i = 0; i < name.Count(); i++)
-                //{
-                //    if (name[i].ID == 1)
-                //    {
-                //        Console.WriteLine($"Ходит {name[i].Name}");
-                //        step1 = minValue++;
-                //        if (step1 == quessNumber)
-                //        {
-                //            Console.WriteLine($"Победил игрок {name[i].Name}");
-                //            Console.ReadLine();
-                //            return;
-                //        }
-                //    }
-                //}
-
-                //for (int i = 0; i < name.Count(); i++)
-                //{
-                //    if (name[i].ID == 2)
-                //    {
-                //        Console.WriteLine($"Ходит {name[i].Name}");
-                //        step2 = rand.Next(minValue, maxValue);
-                //        if (step2 == quessNumber)
-                //        {
-                //            Console.WriteLine($"Победил игрок {name[i].Name}");
-                //            Console.ReadLine();
-                //            return;
-                //        }
-                //    }
-                //}
-
                 for (int i = 0; i < name.Count(); i++)
                 {
+                    if (name[i].ID == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"---Ходит [{name[i].Name}]---");
+                        Console.ResetColor();
+                        step1 = minValue++;
+                        if (step1 == quessNumber)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"Победил игрок [{name[i].Name}] угадавший заданное число: {quessNumber}");
+                            Console.ResetColor();
+                            Console.ReadLine();
+                            return;
+                        }
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Его число {step1}\n");
+                        Console.ResetColor();
+                    }
+
+                    if (name[i].ID == 2)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"---Ходит [{name[i].Name}]---");
+                        Console.ResetColor();
+                        step2 = rand.Next(minValue, maxValue);
+                        if (step2 == quessNumber)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"Победил игрок [{name[i].Name}] угадавший заданное число: {quessNumber}");
+                            Console.ResetColor();
+                            Console.ReadLine();
+                            return;
+                        }
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Его число {step2}\n");
+                        Console.ResetColor();
+                    }
+
                     if (name[i].ID == 3)
                     {
-                        Console.WriteLine($"Ходит {name[i].Name}");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.WriteLine($"---Ходит [{name[i].Name}]---");
+                        Console.ResetColor();
 
-                        
-                        Console.WriteLine($"Его число {step3}");
-
-                        for (int j = 0; minValue < maxValue; j++)
+                        for (int j = temp3; minValue < maxValue; j++)
                         {
                             step3 = rand.Next(minValue, maxValue);
                             if (masForPlayer3.Contains(step3))
@@ -123,36 +149,73 @@ namespace CABuscketGame
                             break;
                         }
 
-                        Console.WriteLine($"Его число {step3}");
-
                         if (step3 == quessNumber)
                         {
-                            Console.WriteLine($"Победил игрок {name[i].Name}");
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            Console.WriteLine($"Победил игрок [{name[i].Name}] угадавший заданное число: {quessNumber}");
+                            Console.ResetColor();
                             Console.ReadLine();
                             return;
                         }
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.WriteLine($"Его число {step3}\n");
+                        Console.ResetColor();
+                    }
+
+                    if (name[i].ID == 4)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine($"---Ходит [{name[i].Name}]---");
+                        Console.ResetColor();
+                        step4 = minValue++;
+                        if (step4 == step1 || step4 == step2 || step4 == step3)
+                        {
+                            step4++;
+                        }
+
+                        if (step4 == quessNumber)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine($"Победил игрок [{name[i].Name}] угадавший заданное число: {quessNumber}");
+                            Console.ResetColor();
+                            Console.ReadLine();
+                            return;
+                        }
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine($"Его число {step4}\n");
+                        Console.ResetColor();
+                    }
+
+                    if (name[i].ID == 5)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine($"---Ходит [{name[i].Name}]---");
+                        Console.ResetColor();
+
+                        step5 = rand.Next(minValue, maxValue);
+                        if (step5 == step1 || step5 == step2 || step5 == step3 || step5 == step4)
+                        {
+                            step5++;
+                        }
+
+                        if (step5 == quessNumber)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.WriteLine($"Победил игрок [{name[i].Name}] угадавший заданное число: {quessNumber}");
+                            Console.ResetColor();
+                            Console.ReadLine();
+                            return;
+                        }
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine($"Его число {step5}\n");
+                        Console.ResetColor();
                     }
                 }
-
-                //for (int i = 0; i < name.Count(); i++)
-                //{
-                //    if (name[i].ID == 4)
-                //    {
-                //        Console.WriteLine($"Ходит {name[i].Name}");
-                //    }
-                //}
-
-                //for (int i = 0; i < name.Count(); i++)
-                //{
-                //    if (name[i].ID == 5)
-                //    {
-                //        Console.WriteLine($"Ходит {name[i].Name}");
-                //    }
-                //}
 
                 Console.WriteLine($"Все персонажи походили. Нажмите Enter для продолжения шага. Шагов: {pass++ + 1}");
                 Console.ReadLine();
             }
+            #endregion
         }
     }
 }
